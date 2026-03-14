6fad4ef0f7bee60773170ba0ea7c19ba19b2ab62
Asabarish <asabarish892@gmail.com>
2026-03-14 16:19:42 +0530

    Success

--- kural/kural/wsgi.py
+++ kural/kural/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for kuran project.
+WSGI config for kural project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kuran.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kural.settings')
 
 application = get_wsgi_application()
